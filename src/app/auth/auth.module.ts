48af9954cc94d4ba4db49c0134f45e02e87ad3a4
Bryiam Rojas <brojas@applaudostudios.dev>
2022-11-04 13:06:00 -0500

    added form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { MaterialModule } from '../material/material.module';
 import { AuthRoutingModule } from './auth-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, MaterialModule, AuthRoutingModule],
+  imports: [CommonModule, MaterialModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.loginForm = fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
 }
