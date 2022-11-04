6e3816218236c4be8038de0f2f0b3f1fd61cc682
Bryiam Rojas <brojas@applaudostudios.dev>
2022-11-03 23:18:17 -0500

    Added some material imports into the Material module and imported it into the auth Module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
 
-const MATERIAL_COMPONENTS = [MatButtonModule];
+const MATERIAL_COMPONENTS = [
+  MatButtonModule,
+  MatCardModule,
+  MatSidenavModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  MatTabsModule,
+];
 
 @NgModule({
   declarations: [],
