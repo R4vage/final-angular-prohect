f3ba84e1afff0b6f2df32532dd4e5c54a8001717
R4vage <jmarc@applaudostudios.dev>
2022-11-10 11:58:02 -0300

    New structure for user-profile

--- src/app/user-profile/user-profile.module.ts
+++ src/app/user-profile/user-profile.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserProfileComponent } from './user-profile.component';
 import { EffectsModule } from '@ngrx/effects';
 import { UserEffects } from './user-profile-store/user.effects';
-
-
+import { UserProfileRestService } from './services/user-profile-rest.service';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @NgModule({
-  declarations: [
-    UserProfileComponent
-  ],
-  imports: [
-    CommonModule,
-    EffectsModule.forFeature([UserEffects]),
-  ]
+  declarations: [UserProfileComponent],
+  imports: [CommonModule, EffectsModule.forFeature([UserEffects])],
+  providers: [UserProfileRestService]
 })
-export class UserProfileModule { }
+export class UserProfileModule {}
