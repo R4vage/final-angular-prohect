9ca97c0ad6d30ac5a94ae0234100721655f1debe
R4vage <jmarc@applaudostudios.dev>
2022-11-10 13:23:27 -0300

    User-profile routing fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
-import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserProfileComponent } from './user-profile/components/user-profile/user-profile.component';
 
 const routes: Routes = [
   {
