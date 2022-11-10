dedd02295d6fe9e0d7e3cd72ae0de17be5924ea0
R4vage <jmarc@applaudostudios.dev>
2022-11-10 14:36:05 -0300

    Cambio routeo

--- src/app/user-profile/user-profile.module.ts
+++ src/app/user-profile/user-profile.module.ts
@@ -4,10 +4,11 @@ import { EffectsModule } from '@ngrx/effects';
 import { UserEffects } from './user-profile-store/user.effects';
 import { UserProfileRestService } from './services/user-profile-rest.service';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { UserProfileRoutingModule } from './user-profile-routing.module';
 
 @NgModule({
   declarations: [UserProfileComponent],
-  imports: [CommonModule, EffectsModule.forFeature([UserEffects])],
+  imports: [CommonModule, EffectsModule.forFeature([UserEffects]), UserProfileRoutingModule],
   providers: [UserProfileRestService]
 })
 export class UserProfileModule {}
