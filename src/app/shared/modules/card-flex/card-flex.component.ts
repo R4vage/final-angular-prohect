68efc4c82290ccf3d61b8dad0a93a3598d0ea7c5
R4vage <jmarc@applaudostudios.dev>
2022-11-12 13:19:54 -0300

    Merge self

--- src/app/shared/modules/card-flex/card-flex.component.ts
+++ src/app/shared/modules/card-flex/card-flex.component.ts
@@ -5,18 +5,12 @@ import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core
   template: `
     <section class="section">
       <h2>{{name}}</h2>
-<<<<<<< HEAD
       <div class="cards">
         <ng-content select=".card"></ng-content>
       </div>
     </section>
   `,
   styleUrls: ['./card-flex.component.scss'],
-=======
-      <ng-content select=".cards"></ng-content>
-    </section>
-  `,
->>>>>>> 4ee285eb3caa31b427bcd630fa33555da358548c
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardFlexComponent{
--- src/app/shared/modules/card-flex/card-flex.module.ts
+++ src/app/shared/modules/card-flex/card-flex.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardFlexComponent } from './card-flex.component';
-<<<<<<< HEAD
 import { MatCardModule } from '@angular/material/card';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TrackCardComponent } from './components/track-card/track-card.component';
@@ -25,20 +24,6 @@ import { MatIconModule } from '@angular/material/icon';
     CardFlexComponent,
     TrackCardComponent,
     ArtistCardComponent
-=======
-
-
-
-@NgModule({
-  declarations: [
-    CardFlexComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports:[
-    CardFlexComponent
->>>>>>> 4ee285eb3caa31b427bcd630fa33555da358548c
   ]
 })
 export class CardFlexModule { }
