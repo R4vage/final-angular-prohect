ab3a74e3b6e857de39de2f546c8e232f4bcacf31
R4vage <jmarc@applaudostudios.dev>
2022-11-13 17:04:52 -0300

    Card for playlist, and included in search

---
+++ src/app/shared/modules/card-flex/components/playlist-card/playlist-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { PlaylistItem } from 'src/app/core/models/playlist.models';
+
+@Component({
+  selector: 'app-playlist-card',
+  templateUrl: './playlist-card.component.html',
+  styleUrls: ['./playlist-card.component.scss']
+})
+export class PlaylistCardComponent implements OnInit {
+  @Input() playlist!: PlaylistItem;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
