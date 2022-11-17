88901395e139a6185d59645eef38f0161ce02f12
R4vage <jmarc@applaudostudios.dev>
2022-11-17 15:56:15 -0300

    Small fix and refactoring in Search Page

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,14 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
-import { AlbumItem, Albums, Artist } from '../core/models/album.models';
-import { PlaylistItem } from '../core/models/playlist.models';
+import { map, Observable } from 'rxjs';
 import { SearchResults } from '../core/models/rest.models';
-import { Track } from '../core/models/track.models';
 import { SearchState } from './search-store/search.reducer';
-import { searchHasBeenDone, selectSearchByValue } from './search-store/search.selectors';
-import { SearchRestService } from './services/search-rest.service';
+import { selectSearchByValue } from './search-store/search.selectors';
 
 @Component({
   selector: 'app-search-page',
@@ -16,27 +12,17 @@ import { SearchRestService } from './services/search-rest.service';
   styleUrls: ['./search-page.component.scss']
 })
 export class SearchPageComponent implements OnInit {
-  searchResults!:SearchResults;
+  searchResults$!:Observable<SearchResults | undefined>;
   searchValue!:string;
-  artists:Artist[] = [];
-  tracks:Track[] = [];
-  albums: AlbumItem[] = [];
-  playlists: PlaylistItem[] = [];
-  resultsSubscription!: Subscription;
   constructor(private route: ActivatedRoute, private store: Store<SearchState>) { }
 
   ngOnInit(): void {
     this.route.params.subscribe({
       next: params => {
         this.searchValue = params['value'];
-        this.resultsSubscription = this.store.select(selectSearchByValue(this.searchValue)).subscribe(results => {
-          if(results) {
-            this.artists = results.results.artists.items;
-            this.albums = results.results.albums.items;
-            this.tracks = results.results.tracks.items;
-            this.playlists = results.results.playlists.items
-          }
-        });
+        this.searchResults$ = this.store.select(selectSearchByValue(this.searchValue)).pipe(
+          map(result => result?.results)
+        );
       } 
     });
   }
